57e616a585c08c16f7985de97ca3ee7a7d7fac3f
ellalearns <ellalearnscopy@gmail.com>
2024-07-09 16:10:31 +0100

    setup react query, connect with backend

--- src/App.js
+++ src/App.js
@@ -1,9 +1,29 @@
 import React from "react";
+import { useQuery } from "react-query";
+import getWelcome from "./api/getWelcome"
+
 
 function App() {
+
+    const { isLoading, isError, error, data } = useQuery({
+        queryKey: "welcome",
+        queryFn: getWelcome
+    })
+
+    if (isLoading) {
+        return <div>loading...</div>
+    }
+
+    if (isError) {
+        return <div>Error, {error.message}</div>
+    }
+
+    const greeting = <p>{data.greeting}</p>
+
     return (
         <div>
             <p>first steps</p>
+            {greeting}
         </div>
     )
 }
