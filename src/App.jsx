815adc2c15f14e3d5da71626025213de96489f4f
ellalearns <ellalearnscopy@gmail.com>
2024-07-17 17:34:54 +0100

    create signup page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Today from "./pages/Today";
-// import "./styles/App.css"
+import SignUp from "./pages/SignUp";
+import "./styles/App.css"
 
 
 function App() {
     return (
         <Routes>
             <Route path="/" element={<Today />} />
+            <Route path="signup" element={<SignUp />} />
         </Routes>
     )
 }
---
+++ src/pages/SignUp.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import "../styles/SignUp.css"
+import icon from "../images/icon.svg"
+
+
+function SignUp() {
+
+    return (
+        <div id="signup-container">
+            <img src={icon} />
+            <h1 id="name-h">AURORA</h1>
+            <form>
+            <div id="input-div">
+                <h4 id="input-h">Make a new Aurora account</h4>
+                <input type="text" id="username" required placeholder="new_username" className="input" />
+                <input type="email" id="email" required placeholder="your email here" className="input" />
+                <input type="password" id="password" required placeholder="new password" className="input" />
+                <input type="password" minLength="8" id="repeatPassword" required placeholder="repeat password" className="input" />
+            </div>
+            <div id="button-div">
+                <button>New Aurora</button>
+                <p>have an account? <span><a href="">sign in</a></span></p>
+            </div>
+            </form>
+        </div>
+    )
+}
+
+
+export default SignUp;
