d8243ceeaded282d89fbe2de3780f1b6499889c1
ellalearns <ellalearnscopy@gmail.com>
2024-07-17 15:58:14 +0100

    setup react-router

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { QueryClient, QueryClientProvider } from "react-query";
+import { BrowserRouter } from "react-router-dom";
 
 const root = document.getElementById("root")
 ReactDOM.createRoot(root).render(
-    <QueryClientProvider client={new QueryClient}>
-        <App />
-    </QueryClientProvider>
+    <BrowserRouter>
+        <QueryClientProvider client={new QueryClient}>
+            <App />
+        </QueryClientProvider>
+    </BrowserRouter>
 )
