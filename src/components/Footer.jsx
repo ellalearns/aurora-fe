8694eeb94497efb90869633cd95d62070b8f45bd
ellalearns <ellalearnscopy@gmail.com>
2024-07-19 13:42:49 +0100

    create modal

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,53 @@
+import { useState } from "react";
 import React from "react";
 import plus from "../images/plus.svg"
 import "../styles/Footer.css"
+import Modal from "./Modal";
 
 
 function Footer() {
 
+    const [isOpen, setIsOpen] = useState(false)
+
+    const showModal = ({ children, mainButtonText }) => {
+        return (
+            <Modal
+                setIsOpen={setIsOpen}
+                mainButtonText={mainButtonText}
+                children={children}
+            />
+        )
+    }
+
+    const newTask = () => {
+        return (
+            <div className="modal-item">
+                <h3 className="modal-title">Create a new Task</h3>
+                <div className="modal-body">
+                    <input placeholder="task name" id="input"></input>
+                    <textarea maxLength="250" placeholder="optional task descripton" id="desc"></textarea>
+                    <div id="check">
+                        <input type="checkbox" name="major" id="checkbox"></input>
+                        <label for="major">major</label>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
     return (
         <div className="footer-nav">
-            <img src={plus} />
+            <img
+                src={plus}
+                onClick={() => { setIsOpen(true) }}
+            />
+            {isOpen && showModal({
+                mainButtonText: "Add New Task",
+                children: newTask()
+            })}
         </div>
     )
-}
+};
 
-export default Footer
+export default Footer;
+
---
+++ src/components/Modal.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from "react";
+import ReactDOM from "react-dom";
+import "../styles/Modal.css";
+import close from "../images/close.svg";
+
+
+function Modal({ setIsOpen, mainButtonText, children }) {
+
+    return ReactDOM.createPortal(
+        <div className="modal-container">
+            <div className="modal">
+                <div className="top-modal">
+                    <img
+                        src={close}
+                        onClick={() => { setIsOpen(false) }}
+                    />
+                </div>
+                <div className="middle-modal">
+                    {children}
+                </div>
+                <div className="bottom-modal">
+                    <button
+                        id="cancel-modal-button"
+                        className="button-modal"
+                        onClick={() => { setIsOpen(false) }}
+                    >Cancel
+                    </button>
+                    <button
+                        id="main-modal-button"
+                        className="button-modal"
+                    >
+                        {mainButtonText}
+                    </button>
+                </div>
+            </div>
+        </div>,
+        document.getElementById("portal")
+    )
+}
+
+export default Modal;
+
